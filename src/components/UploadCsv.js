2001ba0d685d2ddb2bc46a7707f9521bd4834567
Deepak Shrivastava <dpkshrivastava10@gmail.com>
2019-06-27 09:39:24 +0530

    CSV Component Created

--- src/components/UploadCsv.js
+++ src/components/UploadCsv.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Papa from 'papaparse';
-
+import ReactFileReader from 'react-file-reader';
 
 
 
@@ -8,39 +7,20 @@ class CsvComponent extends React.Component {
 
     constructor() {
         super(); 
-        this.state = {
-            csvFile: undefined
-        };
-        this.handleChange = this.handleChange.bind(this);
     }
-
-    handleChange = (event) => {
-        this.setState({
-            csvFile: event.target.files[0]
-        });
-    };
-
-    importCSV = () => {
-        let csvFile = this.state;
-        Papa.parse(csvFile, {
-            complete: this.updateData,
-            harder: true
-        }) ;
-    };
-
-    updateData(result){
-        let data = result.data;
-        console.log(data);
-    }
+    handleFiles = files => {
+        console.log(files)
+      }
     render() {
         return(               
 
                 <div className = "App">
                     <h3>Import CSV File</h3>
-                    <input className = "csv-input" accept = ".csv" type = "file"
-                     onChange = {this.handleChange} />                    
-                    <button onClick = {this.importCSV.bind(this)} className = "btn btn-success">Upload Now </button>    
+                    <ReactFileReader type = "file" handleFiles={this.handleFiles} fileTypes={'.csv'}>
+                        <button className='btn btn-info'>Upload</button>
+                    </ReactFileReader>
                     </div>
+
         );
     }
 }
