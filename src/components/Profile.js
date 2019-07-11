65d7da4a3271648ad079f979ba6debd27e0b2c57
MohdSami-Coder <mhd.sami44@gmail.com>
2019-07-11 16:16:45 +0530

    Changes created by Sami

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,155 +1,47 @@
-import React from "react";
-// import {Provider} from 'react-redux';
-// import {connect} from 'react-redux';
+import React, { useReducer } from 'react';
+import {getUserAccess} from "../store/api/index.js";
+class Profile extends React.Component{
 
-// import {store} from '../store/store.js';
-// import {stateMapper} from '../store/store.js';
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            name: "",
+            email:"",
+            imageUrl:""
+        }
+    }
 
+    componentDidMount(){
+        
+       
 
-class Profile extends React.Component {
+        let user = getUserAccess();
+        
+    
+        this.setState({
+            name: user.name,
+            email: user.email,
+            imageUrl: user.imageUrl
+        })
+    }
 
-  constructor(props){
-    super(props)
-
-    this.state = {
-      name: "",
-      email: ""
-    };
-
-    this.nameEntered = this.nameEntered.bind(this);
-    this.emailEntered = this.emailEntered.bind(this);
-    this.profileSubmit = this.profileSubmit.bind(this); 
-  }
-
-  nameEntered(event){
-    this.setState({
-      name: event.target.value
-    })
-  }
-
-  emailEntered(event){
-    this.setState({
-      email: event.target.value
-    })
-  }
-
-  profileSubmit(){
-    console.log(this.state.name, this.state.email);
-  }
-
-  // renderProfile(){
-  //   return this.props.profile.map(function(c){
-  //     return(
-  //       <p>{c.name}, {c.email}</p>
-  //     )
-  //   });
-  // }
-
-
-  render() {
+render() {
     return (
       <div className="">
-        <h3>PROFILE</h3>
+         <h3>PROFILE</h3>
 
-        <hr />
-
-        <br />
-        <br />
-
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span
-              className="input-group-text bg-primary text-white"
-              id="inputGroup-sizing-default"
-            >
-              Full Name
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Full Name"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </div>
-
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span
-              className="input-group-text bg-primary text-white"
-              id="inputGroup-sizing-default"
-            >
-              Email
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Email"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </div>
-
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span
-              className="input-group-text bg-primary text-white"
-              id="inputGroup-sizing-default"
-            >
-              Address
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Address"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </div>
-
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span
-              className="input-group-text bg-primary text-white"
-              id="inputGroup-sizing-default"
-            >
-              City
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="City"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </div>
-
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span
-              className="input-group-text bg-primary text-white"
-              id="inputGroup-sizing-default"
-            >
-              State
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="State"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </div>
-      </div>
+         <hr />
+        
+             <img src={this.state.imageUrl} class="img-thumbnail" alt="Profile_Image" width="100" height="100"/>
+   
+          <br/>
+          <br/>  
+           <p><strong>Name:&nbsp;</strong><strong>{this.state.name}</strong></p>
+ 
+           <p><strong>Email:&nbsp;</strong><strong>{this.state.email}</strong></p>
+       </div>
     );
   }
 }
-
-// let Profile = connect(stateMapper)(ProfileComponent)
-
 export default Profile;
