98a797e83b148b1554b89e067187095661bcb27a
MohdSami-Coder <mhd.sami44@gmail.com>
2019-07-11 16:04:38 +0530

    Changes created by Sami

--- src/components/ContentArea.js
+++ src/components/ContentArea.js
@@ -5,12 +5,7 @@ import ChartData from "./ChartData";
 import Dashboard from "./Dashboard";
 import CSVFileUploader from "./CsvFileUploader.js";
 
-// import LineChart from './LineChart.js';
-// import AreaChart from './AreaChart.js';
-// import BarChart from './BarChart.js';
-// import PieChart from './PieChart.js';
 import CreateChart from "./CreateChart.js";
-// import DataType from "./DataType.js";
 import Profile from "./Profile.js";
 
 import Logout from "./logout.js";
@@ -26,20 +21,10 @@ class ContentArea extends React.Component {
         <div className="container m-5"> 
           <Route path="/app/dashboard"  component={Dashboard} />
           <Route path="/app/createchart"  component={CreateChart} />
-          {/* <Route path="/app/datatype"  component={DataType} /> */}
-
-
           <Route path="/app/make-your-chart" component={ChartData} />
           <Route path="/app/uploadCsv" component={CSVFileUploader} />
-
-          {/* <Route path="/app/linechart" component={LineChart} />
-          <Route path="/app/areachart" component={AreaChart} />
-          <Route path="/app/barchart" component={BarChart} />
-          <Route path="/app/piechart" component={PieChart} /> */}
-
           <Route path="/app/profile" component={Profile} />
           <Route path="/app/logout" component= {Logout} />
-
           <Route path="/app/logout" component={Logout} />
 
         </div>
