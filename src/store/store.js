ef55a28ad4807cbd33c817550c219ad2e27e3058
Mohit Mojo <mohit.mkj06@gmail.com>
2019-07-16 13:54:39 +0530

    minor fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,41 +1,6 @@
 import { createStore, combineReducers } from "redux";
 import updateChartReducer from "./reducers/updateChartReducer";
 
-// let reducer = function(oldState, action) {
-//   let initialState = {
-//     isChartLoading: false
-//   };
-
-//   if (!oldState) {
-//     return initialState;
-//   }
-
-//   let newState = initialState;
-
-//   if (action.type == "FETCH_CHART") {
-//     // fetch("https://dog.ceo/api/breeds/list/all")
-//     //     .then(function(response) {
-//     //         return response.json();
-//     //     })
-//     //     .then(function(data) {
-//     //         store.dispatch({
-//     //             type: "MENU_LOADED",
-//     //             data: Object.keys(data.message)
-//     //         });
-//     //     })
-//     //     .catch(function(err){
-//     //         console.log(err);
-//     //     });
-
-//     newState.isChartLoading = true;
-//   }
-
-//   if (action.type == "CHART_LOADED") {
-//     newState.breeds = action.data;
-//   }
-//   return newState;
-// };
-
 let rootReducer = combineReducers({
   updateChart: updateChartReducer
 });
