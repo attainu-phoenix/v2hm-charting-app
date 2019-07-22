9261808258a6aa5a0c7a21341aff966539ce3ba2
Ansal <ansal@bssatech.com>
2019-07-22 17:17:14 +0530

    reverted index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-// import Login from "./components/Login.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Login from "./components/Login.js";
 
-// import Landing from "./components/Landing.js";
-// import App from "./components/App.js";
-
-// import {store} from "./store/store.js";
-
-// class Home extends React.Component {
-//   doRedirect() {
-//     let loggedIn = localStorage.getItem("user");
-
-//     if (loggedIn) {
-//       return <Redirect to="/app/dashboard" />;
-//     } else {
-//       return <Redirect to="/" />;
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <Router>
-//         <Route path="/" exact={true} component={Landing} />
-//         <Route path="/login" component={Login} />
-//         <Route path="/app" component={App} />
-//         {this.doRedirect()}
-//       </Router>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<Home />, document.getElementById("root"));
-
+import Landing from "./components/Landing.js";
+import App from "./components/App.js";
 
 import {store} from "./store/store.js";
+
+class Home extends React.Component {
+  doRedirect() {
+    let loggedIn = localStorage.getItem("user");
+
+    if (loggedIn) {
+      return <Redirect to="/app/dashboard" />;
+    } else {
+      return <Redirect to="/" />;
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Route path="/" exact={true} component={Landing} />
+        <Route path="/login" component={Login} />
+        <Route path="/app" component={App} />
+        {this.doRedirect()}
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<Home />, document.getElementById("root"));
