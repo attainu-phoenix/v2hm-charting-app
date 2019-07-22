9db7e576cbf6aeaa761a24df4f1c7509606887d1
Ansal <ansal@bssatech.com>
2019-07-22 16:22:52 +0530

    removed all reducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,7 @@
 import { createStore, combineReducers } from "redux";
-import updateChartReducer from "./reducers/updateChartReducer";
-import ChartReducer from "./reducers/ChartReducer.js";
-import saveChartsReducer from "./reducers/saveChartsReducer.js";
 
 
 let reducer = combineReducers({
-  updateChart: updateChartReducer,
-  chartData: ChartReducer,
-  charts: saveChartsReducer
 });
 let store = createStore(reducer);
 
