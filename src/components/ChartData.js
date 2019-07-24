f4bce83e5a79ab8d612ffca3d9fb8f689cd7d0ea
Ansal <ansal@bssatech.com>
2019-07-24 16:40:32 +0530

    testing chart component

--- src/components/ChartData.js
+++ src/components/ChartData.js
@@ -10,6 +10,59 @@ class ChartDataComponent extends React.Component {
     super(props);
 
     this.saveChartHandle = this.saveChartHandle.bind(this);
+
+    this.options = {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: "line"
+      },
+      title: {
+        text: "Export sales January, 2019"
+      },
+      tooltip: {
+        pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>"
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: "pointer",
+          dataLabels: {
+            enabled: false
+          },
+          showInLegend: true
+        }
+      },
+      series: [
+        {
+          name: "Brands",
+          colorByPoint: true,
+          data: [
+            {
+              name: "Coca Cola",
+              y: 35
+            },
+            {
+              name: "Red Bull",
+              y: 15
+            },
+            {
+              name: "Pepsi",
+              y: 25
+            },
+            {
+              name: "Kingfisher",
+              y: 15
+            },
+            {
+              name: "Carlsberg",
+              y: 10
+            }
+          ]
+        }
+      ]
+    };
   }
 
   // componentDidMount() {
@@ -58,7 +111,7 @@ class ChartDataComponent extends React.Component {
             <button className="btn btn-primary mx-3">Reset Fields</button>
           </div>
           <div className="my-4">
-            <ChartComponent options={this.props.updateChart.data} />
+            <ChartComponent options={this.options} />
           </div>
         </div>
       </div>
